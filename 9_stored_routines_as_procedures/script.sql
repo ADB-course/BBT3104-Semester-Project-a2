f7ddcbe688e684a52dbf6a767230171628cf7e29
Arsene ndimanya <146384225+Arsenendimanya151311@users.noreply.github.com>
2024-11-21 13:42:10 +0300

    Update script.sql
    Signed-off-by: Arsene ndimanya  <146384225+Arsenendimanya151311@users.noreply.github.com>

--- 9_stored_routines_as_procedures/script.sql
+++ 9_stored_routines_as_procedures/script.sql
@@ -1,1 +1,16 @@
--- Write your SQL code here
+CREATE PROCEDURE UpdateOrderStatus(IN orderID INT, IN newStatus VARCHAR(20))
+BEGIN
+  UPDATE Order
+  SET OrderStatus = newStatus
+  WHERE OrderID = orderID;
+END;
+
+
+10.6 Stored Routines as Functions
+CREATE FUNCTION GetExpiringCementsReport(cutoffDate DATE)
+RETURNS TABLE
+AS
+RETURN
+  SELECT CementID, CementName, CementType, ExpiryDate
+  FROM Product
+  WHERE ExpiryDate <= cutoffDate;
